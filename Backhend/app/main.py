87a31a0d5340ab8e1a9d6431e9c45e5726bbf681
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-06-25 14:39:58 +0530

    Added new end point

--- Backhend/app/main.py
+++ Backhend/app/main.py
@@ -1,17 +1,33 @@
-from fastapi import FastAPI, Depends, Query
+from fastapi import FastAPI, Depends, Query, HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy import and_, or_
 from datetime import datetime
 
 from app.database import engine, get_db
 from app.models import Base, Product
+from app.Schema import ProductPage
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-@app.get("/products")
+@app.get("/")
+def root():
+    return {
+        "message": "Product Catalog API",
+        "total_products": "200000+"
+    }
+
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
+
+
+@app.get("/products", response_model=ProductPage)
 def get_products(
     limit: int = Query(20, le=100),
     cursor: str | None = None,
@@ -26,19 +42,25 @@ def get_products(
 
     # Cursor pagination
     if cursor:
-        timestamp_str, product_id = cursor.split("|")
+        try:
+            timestamp_str, product_id = cursor.split("|")
+            timestamp = datetime.fromisoformat(timestamp_str)
 
-        timestamp = datetime.fromisoformat(timestamp_str)
-
-        query = query.filter(
-            or_(
-                Product.updated_at < timestamp,
-                and_(
-                    Product.updated_at == timestamp,
-                    Product.id < int(product_id)
+            query = query.filter(
+                or_(
+                    Product.updated_at < timestamp,
+                    and_(
+                        Product.updated_at == timestamp,
+                        Product.id < int(product_id)
+                    )
                 )
             )
-        )
+
+        except Exception:
+            raise HTTPException(
+                status_code=400,
+                detail="Invalid cursor format"
+            )
 
     # Fetch products
     products = (
@@ -62,3 +84,28 @@ def get_products(
         "products": products,
         "next_cursor": next_cursor
     }
+
+
+@app.put("/products/{product_id}")
+def update_product(
+    product_id: int,
+    db: Session = Depends(get_db)
+):
+    product = (
+        db.query(Product)
+        .filter(Product.id == product_id)
+        .first()
+    )
+
+    if not product:
+        raise HTTPException(
+            status_code=404,
+            detail="Product not found"
+        )
+
+    product.updated_at = datetime.utcnow()
+
+    db.commit()
+    db.refresh(product)
+
+    return product
